63da7fe301838b44a41231b204bd8313a7c72a17
HamzaJsr <gestiondev.j@gmail.com>
2024-04-12 01:27:54 +0200

    first commit Quiz JS

---
+++ main.js
@@ -0,0 +1,6 @@
+import "./style.css";
+import { Questions } from "./questions";
+
+const app = document.getElementById("app");
+const startButton = document.querySelector("#start");
+startButton.addEventListener("click", () => {});
