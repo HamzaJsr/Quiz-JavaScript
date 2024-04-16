d77519513919089f31de98c0018dd6dfb6e985cc
HamzaJsr <gestiondev.j@gmail.com>
2024-04-16 02:05:17 +0200

    Ajout des reponses

--- main.js
+++ main.js
@@ -11,7 +11,6 @@ function startQuiz(event) {
   let currentQuestion = 0;
   let score = 0;
 
-  clean();
   displayQuestion(currentQuestion);
 
   function clean() {
@@ -21,6 +20,7 @@ function startQuiz(event) {
   }
 
   function displayQuestion(index) {
+    clean();
     const question = Questions[index];
 
     if (!question) {
@@ -29,6 +29,45 @@ function startQuiz(event) {
 
     const title = getTitleElement(question.question);
     app.appendChild(title);
+    const answersDiv = createAnswers(question.answers);
+    app.appendChild(answersDiv);
+    const submitButton = document.createElement("button");
+    submitButton.innerText = "Envoyer";
+    app.appendChild(submitButton);
+
+    submitButton.addEventListener("click", submit);
+  }
+
+  function submit() {
+    const selectedAnswer = app.querySelector('input[name="answer"]:checked');
+    const value = selectedAnswer.value;
+    const question = Questions[currentQuestion];
+    const isCorrect = question.correct === value;
+
+    if (isCorrect) {
+      score++;
+    }
+
+    showFeedBack(isCorrect, question.correct, value);
+    const feedBack = getFeedBackMessage(isCorrect, question.correct);
+    app.appendChild(feedBack);
+
+    setTimeout(() => {
+      currentQuestion++;
+      displayQuestion(currentQuestion);
+    }, 1000);
+  }
+
+  function createAnswers(answers) {
+    const answersDiv = document.createElement("div");
+
+    answersDiv.classList.add("answers");
+
+    for (const answer of answers) {
+      const label = getAnswerElement(answer);
+      answersDiv.appendChild(label);
+    }
+    return answersDiv;
   }
 }
 
@@ -36,4 +75,45 @@ function getTitleElement(text) {
   const title = document.createElement("h3");
   title.innerText = text;
   return title;
+}
+
+function formatId(text) {
+  return text.replaceAll(" ", "-").toLowerCase();
+}
+
+function getAnswerElement(text) {
+  const label = document.createElement("label");
+  label.innerText = text;
+  const input = document.createElement("input");
+  const id = formatId(text);
+  input.id = id;
+  label.htmlFor = id;
+  input.setAttribute("type", "radio");
+  input.setAttribute("name", "answer");
+  input.setAttribute("value", text);
+
+  label.appendChild(input);
+  return label;
+}
+
+function showFeedBack(isCorrect, correct, answer) {
+  const correctAnswerId = formatId(correct);
+  const correctElement = document.querySelector(
+    `label[for="${correctAnswerId}"]`
+  );
+
+  const selectedAnswerId = formatId(answer);
+  const selectedElement = document.querySelector(
+    `label[for="${selectedAnswerId}"]`
+  );
+
+  correctElement.classList.add("correct");
+  selectedElement.classList.add(isCorrect ? "correct" : "incorrect");
+}
+function getFeedBackMessage(isCorrect, correct) {
+  const paragraph = document.createElement("p");
+  paragraph.innerText = isCorrect
+    ? "Bravo tu as eu la bonne reponse"
+    : `Desolé... mais la bonne reponse etait ${correct} `;
+  return paragraph;
 }
