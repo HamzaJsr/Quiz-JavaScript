18da8c1f8690b9183ed9fa7bdff0bb8f7871a2b4
HamzaJsr <gestiondev.j@gmail.com>
2024-04-18 20:51:13 +0200

    Projet fini

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 import "./style.css";
 import { Questions } from "./questions";
 
+const TIMEOUT = 4000;
 const app = document.getElementById("app");
 const startButton = document.querySelector("#start");
 
@@ -17,6 +18,8 @@ function startQuiz(event) {
     while (app.firstElementChild) {
       app.firstElementChild.remove();
     }
+    const progress = displayProgressBar(Questions.length, currentQuestion);
+    app.appendChild(progress);
   }
 
   function displayQuestion(index) {
@@ -24,7 +27,8 @@ function startQuiz(event) {
     const question = Questions[index];
 
     if (!question) {
-      // Finish quiz
+      displayFinishMessage();
+      return;
     }
 
     const title = getTitleElement(question.question);
@@ -38,8 +42,18 @@ function startQuiz(event) {
     submitButton.addEventListener("click", submit);
   }
 
+  function displayFinishMessage() {
+    const h1 = document.createElement("h1");
+    h1.innerText = "Bravo tu as terminé le quiz.";
+    const p = document.createElement("p");
+    p.innerText = `Tu as eu ${score} sur ${Questions.length} points ! `;
+    app.appendChild(h1);
+    app.appendChild(p);
+  }
+
   function submit() {
     const selectedAnswer = app.querySelector('input[name="answer"]:checked');
+    disableAnswers();
     const value = selectedAnswer.value;
     const question = Questions[currentQuestion];
     const isCorrect = question.correct === value;
@@ -49,13 +63,12 @@ function startQuiz(event) {
     }
 
     showFeedBack(isCorrect, question.correct, value);
-    const feedBack = getFeedBackMessage(isCorrect, question.correct);
-    app.appendChild(feedBack);
-
-    setTimeout(() => {
+    displayNextQuestionButton(() => {
       currentQuestion++;
       displayQuestion(currentQuestion);
-    }, 1000);
+    });
+    const feedBack = getFeedBackMessage(isCorrect, question.correct);
+    app.appendChild(feedBack);
   }
 
   function createAnswers(answers) {
@@ -99,12 +112,12 @@ function getAnswerElement(text) {
 function showFeedBack(isCorrect, correct, answer) {
   const correctAnswerId = formatId(correct);
   const correctElement = document.querySelector(
-    `label[for="${correctAnswerId}"]`
+    `label[for='${correctAnswerId}']`
   );
 
   const selectedAnswerId = formatId(answer);
   const selectedElement = document.querySelector(
-    `label[for="${selectedAnswerId}"]`
+    `label[for='${selectedAnswerId}']`
   );
 
   correctElement.classList.add("correct");
@@ -116,4 +129,51 @@ function getFeedBackMessage(isCorrect, correct) {
     ? "Bravo tu as eu la bonne reponse"
     : `Desolé... mais la bonne reponse etait ${correct} `;
   return paragraph;
+}
+
+function displayProgressBar(max, value) {
+  const progress = document.createElement("progress");
+  progress.setAttribute("max", max);
+  progress.setAttribute("value", value);
+  return progress;
+}
+
+function displayNextQuestionButton(callback) {
+  let remainingtimeout = TIMEOUT;
+
+  app.querySelector("button").remove();
+
+  const getButtonText = () =>
+    `Question suivante dans ${remainingtimeout / 1000}s`;
+
+  const nextButton = document.createElement("button");
+  nextButton.innerText = getButtonText();
+  app.appendChild(nextButton);
+
+  const interval = setInterval(() => {
+    remainingtimeout -= 1000;
+    nextButton.innerText = getButtonText();
+  }, 1000);
+
+  const handleNextQuestion = () => {
+    clearInterval(interval);
+    clearTimeout(timeout);
+    callback();
+  };
+
+  const timeout = setTimeout(() => {
+    handleNextQuestion();
+  }, TIMEOUT);
+
+  nextButton.addEventListener("click", () => {
+    handleNextQuestion();
+  });
+}
+
+function disableAnswers() {
+  const radioInputs = document.querySelectorAll('input[type="radio"]');
+
+  for (const radio of radioInputs) {
+    radio.disabled = true;
+  }
 }
